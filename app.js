b318de0fa86ef23e58500cbba728a8cabb05e782
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2024-01-19 11:33:39 +0000

    appChange

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
         document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight);
         document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight);
-        
+
     });
 
     $("#you").click(function (e) {
